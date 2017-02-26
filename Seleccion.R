9c921bfbe1acadf3f5a6c65c32b5201d9850e8a7
derek-corcoran-barrios <gaiterodelsurdelmundo@gmail.com>
2017-02-26 16:48:46 -0600

    <restand>

--- Seleccion.R
+++ Seleccion.R
@@ -34,15 +34,11 @@ colnames(BD) <- gsub("%", "pct", colnames(BD))
 
 
 library(ggplot2)
-
-ggplot(BD, aes(x = ThreePpct, y = MPG)) + geom_point(aes(color = Season)) + geom_smooth(aes(fill = Season))
-ggplot(BD, aes(x = ThreePA, y = MPG)) + geom_point(aes(color = Season)) + geom_smooth(method = "lm",aes(fill = Season))
-
 library(MuMIn)
 library(caret)
 
-preprocov <- preProcess(BD[,-c(1:9)])
-BD[,-c(1:9)] <- predict(preprocov, BD[,-c(1:9)])
+preprocov <- preProcess(BD[,-c(1:9,33)])
+BD[,-c(1:9,33)] <- predict(preprocov, BD[,-c(1:9,33)])
 BD$ThreePpct[is.na(BD$ThreePpct)] <- min(BD$ThreePpct, na.rm = TRUE)
 
 Vars<- BD[,c(13,15,17,20,21,22,23,24,26,28:31)]
@@ -79,6 +75,11 @@ library(reshape2)
 
 Models <- Models[,-1]
 
+#how many nonNA per column:
+
+colSums(!is.na(Models))
+rowSums(!is.na(Models))
+
 dfm <- melt(Models, id.vars="Season")
 dfm$sign <- ifelse(dfm$value > 0 , "+" , ifelse(dfm$value < 0, "-", ""))
 
@@ -92,3 +93,46 @@ dfm2 <- dplyr::filter(dfm, variable == "AST" | variable == "TRB" | variable == "
 
 ggplot(data=dfm2, aes(x=Season, y=abs(value), colour=variable)) +geom_point() + geom_smooth(aes(fill = variable))
 
+
+###############LImiting Variables
+
+
+Models2 <- list()
+
+options(na.action = "na.fail")
+
+
+for(i in 1:length(unique(BD$Season))){
+  temp <- dplyr::filter(BD, Season == unique(BD$Season)[i])
+  Vars<- BD[,c(13,15,17,20,21,22,23,24,26,28:31)]
+  smat <- abs(cor(Vars)) <= .7
+  smat[!lower.tri(smat)] <- NA
+  Model <- glm(MPG~ TwoPA + ThreePA + FTA+ TRB +  AST + STL + BLK +  TOV + PTS +  TwoPpct + ThreePpct + eFGpct + FTpct, data = temp)
+  Select.Model <- dredge(Model, subset = smat,m.lim = c(0, 3))
+  Best.Model<-get.models(Select.Model, 1)[[1]]
+  Models2[[i]] <- data.frame(matrix(summary(Best.Model)$coefficients[,1], ncol = length(summary(Best.Model)$coefficients[,1]), nrow = 1))
+  colnames(Models2[[i]]) <- rownames(summary(Best.Model)$coefficients)
+  Models2[[i]]$Season <- unique(BD$Season)[i]
+}
+
+Models2 <- Reduce(function(x,y) merge(x, y, all = TRUE), Models2)
+
+Models2 <- dplyr::arrange(Models2, Season)
+View(Models2)
+
+
+
+library(reshape2)
+
+Models2 <- Models2[,-1]
+
+dfm <- melt(Models2, id.vars="Season")
+dfm$sign <- ifelse(dfm$value > 0 , "+" , ifelse(dfm$value < 0, "-", ""))
+
+
+p <- ggplot(dfm, aes(x=variable, y=Season)) +  geom_tile(aes(fill=value), colour = "black") + scale_fill_gradient2(low = "blue", high = 'red', name="Strength") + theme(axis.text.x = element_text(angle = 50, hjust = 1, vjust = 1))
+p
+
+dfm2 <- dplyr::filter(dfm, variable == "AST" | variable == "TRB" | variable == "PTS")
+
+ggplot(data=dfm2, aes(x=Season, y=abs(value), colour=variable)) +geom_point() + geom_smooth(aes(fill = variable))
